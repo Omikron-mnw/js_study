664437a29027d9b7339c23a0cf4721619fe3fc48
Omikron-mnw <minomushi.ff15.07.08@gmail.com>
2023-08-16 00:16:41 +0900

    030-920

--- 030_スコープ/920_修了確認_クロージャー/start/main.js
+++ 030_スコープ/920_修了確認_クロージャー/start/main.js
@@ -15,3 +15,68 @@
  * ※前に行った計算結果をもとに四則演算を行います。
  * ※四則演算は"+","-","*","/"を数値ではさんで計算を行います。
  */
+
+// function calcFactory(num) {
+
+//   function plus(val) {
+//     return num + val;
+//   }
+
+//   function minus(val) {
+//     return num - val;
+//   }
+
+//   function multiply(val) {
+//     return num * val;
+//   }
+
+//   function divide(val) {
+//     return num / val;
+//   }
+
+//   return plus, minus, multiply, divide;
+// }
+
+// const calc = calcFactory(10);
+
+// const plus = calc.plus(5);
+// const minus = calc.minus(3);
+// const multiply = calc.multiply(3);
+// const divide = calc.divide(2);
+
+// console.log(plus);
+// console.log(minus);
+// console.log(multiply);
+// console.log(divide);
+
+function calcFactory(val) {
+  return {
+    plus: function (target) {
+      const result = val + target;
+      console.log(val + " + " + target + " = " + result)
+      val = result;
+    },
+    minus: function (target) {
+      const result = val - target;
+      console.log(val + " + " + target + " = " + result)
+      val = result;
+    },
+    multiply: function (target) {
+      const result = val * target;
+      console.log(val + " * " + target + " = " + result)
+      val = result;
+    },
+    divide: function (target) {
+      const result = val / target;
+      console.log(val + " / " + target + " = " + result)
+      val = result;
+    }
+  };
+}
+
+const calc = calcFactory(10);
+
+calc.plus(5);
+calc.minus(3);
+calc.multiply(3);
+calc.divide(2);
