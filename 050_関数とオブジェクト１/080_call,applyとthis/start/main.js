6bb0a7c15de6294618eae19a70385c02343aec80
Omikron-mnw <minomushi.ff15.07.08@gmail.com>
2023-10-03 00:05:59 +0900

    050-080

--- 050_関数とオブジェクト１/080_call,applyとthis/start/main.js
+++ 050_関数とオブジェクト１/080_call,applyとthis/start/main.js
@@ -2,6 +2,17 @@ function a(name) {
     console.log('hello ' + name);
 }
 
-const b = a.bind(null, 'Tim');
+const tim = {name: 'Tim'};
+
+const b = a.bind(tim);
 
 b();
+
+a.apply(tim, ['Tim', 'Bob']);
+a.call(tim, 'Tim', 'Bob');
+
+const arry = [1, 2, 3, 4, 5];
+
+// const result = Math.max.apply(null, Uint8ClampedArray);
+const result = Math.max(...arry);
+console.log(result);
